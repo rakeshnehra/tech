edd57fd566d83af7aff8175d33e3a78515a702f2
rakesh <rakesh>
2023-10-30 23:34:02 +0530

    11fun with file add

--- demo/test.js
+++ demo/test.js
@@ -3,7 +3,7 @@ let arr = [1,2,3,5,4]
 arr.push(5.0)
 
 arr.push('7')
-arr.pop(1)
+arr.pop(1) //last desite delete
 
 
 arr[10] = 11
@@ -11,3 +11,22 @@ arr[10] = 11
 console.log(arr);
 console.log(arr.length);
 console.log(arr[2]);
+
+let  obj = {
+    name: "Ram",
+    id: 23
+}
+
+
+console.log(typeof obj);
+
+
+let  obj1 = {
+    name: "Ram",
+    id: 23,
+    fullname: "Ram raj",
+    newid: 231
+}
+
+
+console.log(obj1);
